66f7093886add0d5c2db301a5b03d1623dfb4a94
ttymed <tyler.boright@gmail.com>
2017-01-20 15:39:06 +0800

    writes the first test for the first method in the class and sets up the structure for the second one

--- spec/csv_tool_spec.rb
+++ spec/csv_tool_spec.rb
@@ -1,11 +1,18 @@
 require "spec_helper"
 
 describe CsvTool do
-  it "has a version number" do
-    expect(CsvTool::VERSION).not_to be nil
+  describe '#convert_to_hash_array' do
+    it 'converts the CSV array to an array of hashes' do
+      csv_array = [['First name', 'Last name'], ['tyler', 'b']]
+      expected_hash_array = [{'First name' => 'tyler', 'Last name' => 'b'}]
+      hash_array = CsvTool::convert_to_hash_array(csv_array)
+      expect(hash_array).to eq(expected_hash_array)
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '#remove_column' do
+    it 'removes a column from the CSV file based on a string' do
+           
+    end
   end
 end
