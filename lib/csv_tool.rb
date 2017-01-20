51053f66ff92acbf263a097abcf8ea6440eefc82
ttymed <tyler.boright@gmail.com>
2017-01-20 16:00:41 +0800

    adds a remove column method and passing test that removes the column from the csv file

--- lib/csv_tool.rb
+++ lib/csv_tool.rb
@@ -6,4 +6,12 @@ class CsvTool
     fields = csv_array.shift
     csv_array.collect { |record| Hash[*fields.zip(record).flatten ] }
   end
+
+  def self.remove_column(csv_hash_array, column_name)
+    csv_hash_array.each do |row|
+      if row.has_key?(column_name)
+        row.delete(column_name)
+      end
+    end
+  end
 end
--- spec/csv_tool_spec.rb
+++ spec/csv_tool_spec.rb
@@ -1,9 +1,10 @@
 require "spec_helper"
 
 describe CsvTool do
+  let(:csv_array){ [['First name', 'Last name'], ['tyler', 'b']] }
+  let(:csv_hash_array){[{'First name' => 'tyler', 'Last name' => 'b'}]}
   describe '#convert_to_hash_array' do
     it 'converts the CSV array to an array of hashes' do
-      csv_array = [['First name', 'Last name'], ['tyler', 'b']]
       expected_hash_array = [{'First name' => 'tyler', 'Last name' => 'b'}]
       hash_array = CsvTool::convert_to_hash_array(csv_array)
       expect(hash_array).to eq(expected_hash_array)
@@ -11,8 +12,15 @@ describe CsvTool do
   end
 
   describe '#remove_column' do
-    it 'removes a column from the CSV file based on a string' do
-           
+    it 'removes a column from the CSV hash array' do
+      expected_hash_array = [{'Last name' => 'b'}]
+      hash_array = CsvTool::remove_column(csv_hash_array, 'First name')
+      expect(hash_array).to eq(expected_hash_array)
+    end
+  end
+
+  describe '#remove_row_with_value' do
+    it 'removes the row from the csv hash array that contains a certain value' do
     end
   end
 end
